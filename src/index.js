5071949114f137b89606f2472a0228991de70d8c
Daryna-91 <darynalytvynenko@gmail.com>
2022-07-01 19:13:40 +0300

    5-days forecast added

--- src/index.js
+++ src/index.js
@@ -50,6 +50,28 @@ let shortDate = document.querySelector("#day");
 shortDate.innerHTML = currentDate(today, true);
 currentDate1.innerHTML = currentDate(today, false);
 let cityName = document.querySelector("#cityName");
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Fri", "Sat", "Sun", "Mon", "Tue"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-1">
+    <div class="card h-100">
+      <div class="card-body-1">
+        <h5 class="card-title">${day}</h5>
+        <i class="fa-solid fa-sun"></i>
+        <p class="card-text">30°C / 18°C</p>
+      </div>
+    </div>
+      </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+displayForecast();
 function form(event) {
   event.preventDefault();
 
@@ -76,8 +98,13 @@ function tempFahrenheit(event) {
 }
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", tempFahrenheit);
-
+function getForecast(coordinates) {
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=5f472b7acba333cd8a035ea85a0d4d4c&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(showTemperature);
+}
 function showTemperature(response) {
+  console.log(response.data.daily);
   let temperature = Math.round(response.data.main.temp);
   console.log(temperature);
   console.log(response);
@@ -98,6 +125,7 @@ function showTemperature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  getForecast(response.data.coord);
 }
 
 let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
